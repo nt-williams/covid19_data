2dcc2b0a04c436377d572249fd374a5264fb2710
Nick Williams <ntwilliams.personal@gmail.com>
2020-03-19 22:02:47 -0400

    changing rds to csv

--- hopkins_data.R
+++ hopkins_data.R
@@ -16,8 +16,13 @@ fun_in <- here("functions", "hopkins_data_clean.R")
 cc_in <- "https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_time_series/time_series_19-covid-Confirmed.csv"
 dths_in <- "https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_time_series/time_series_19-covid-Deaths.csv"
 rcvr_in <- "https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_time_series/time_series_19-covid-Recovered.csv"
-all_US_out <- here("extracted", paste0("daily_US_count_", as.character(Sys.Date()), ".rds"))
-all_USL_out <- here("extracted", paste0("daily_US_locale_count_", as.character(Sys.Date()), ".rds"))
+
+all_US_cc_out <- here("extracted", paste0("daily_confirmed_cases_US_count_", as.character(Sys.Date()), ".csv"))
+all_US_dths_out <- here("extracted", paste0("daily_deaths_US_count_", as.character(Sys.Date()), ".csv"))
+all_US_rcvr_out <- here("extracted", paste0("daily_recovered_US_count_", as.character(Sys.Date()), ".csv"))
+all_USL_cc_out <- here("extracted", paste0("daily_confirmed_cases_US_locale_count_", as.character(Sys.Date()), ".csv"))
+all_USL_dths_out <- here("extracted", paste0("daily_deaths_US_locale_count_", as.character(Sys.Date()), ".csv"))
+all_USL_rcvr_out <- here("extracted", paste0("daily_recovered_US_locale_count_", as.character(Sys.Date()), ".csv"))
 
 # data import -------------------------------------------------------------
 
@@ -42,7 +47,13 @@ all_US_locale <- map(., pull_US_locale)
 
 # export ------------------------------------------------------------------
 
-walk2(list(all_US, all_US_locale), 
-      list(all_US_out, all_USL_out), 
-      saveRDS)
+# total US cases by day
+walk2(all_US, 
+      list(all_US_cc_out, all_US_dths_out, all_US_rcvr_out), 
+      write.csv, row.names = F)
+
+# US cases by locale by day
+walk2(all_US_locale, 
+      list(all_USL_cc_out, all_USL_dths_out, all_USL_rcvr_out), 
+      write.csv, row.names = F)
 
